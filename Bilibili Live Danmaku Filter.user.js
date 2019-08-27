06937a45377ae7155b0553b66490b98a80dca3aa
yuyuyzl <yuzhongliang22@163.COM>
2019-08-27 17:59:47 +0800

    0.5.2增加了斗鱼支持

--- Bilibili Live Danmaku Filter.user.js
+++ Bilibili Live Danmaku Filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili Live Danmaku Filter
 // @namespace    http://tampermonkey.net/
-// @version      0.5.1
+// @version      0.5.2
 // @description  使用一个简单的定时器把弹幕按照给定的正则表达式过滤一遍
 // @supportURL   http://nga.178.com/read.php?tid=17690584
 // @author       yuyuyzl
@@ -13,6 +13,7 @@
 // @grant        GM_setClipboard
 // @grant        GM_info
 // @match        *://live.bilibili.com/*
+// @match        *://www.douyu.com/*
 // @match        *://yuyuyzl.github.io/BiliDMFilter/*
 // @match        *://localhost*BiliDMFilter/*
 // ==/UserScript==
@@ -163,6 +164,118 @@ var config={
         }, 100);
 
     }else
+    if(window.location.href.match(/.*\/www.douyu.com\/.*/)){
+        var main=setInterval(function(){
+            var danmuClassName="";
+            $("div").each((i,o)=>{
+                if($(o).attr("class") && $(o).attr("class").trim().substr(0,6)=="danmu-")danmuClassName=$(o).attr("class").trim();
+            })
+            if(danmuClassName!="")clearInterval(main);else return;
+            console.log(danmuClassName);
+            // 以下CSS以及字幕框元素来自SOW社团的自动字幕组件
+            // 发布帖链接：http://nga.178.com/read.php?tid=17180967
+            $("head").append('<style type="text/css">\n' +
+                '    .SubtitleBody{height:160px;background-color:rgba(0, 0, 0, 0.8);color:#fff;}\n' +
+                '    .SubtitleBody.mobile{position:relative;top:5.626666666666667rem;}\n' +
+                '    .SubtitleBody .title{padding:10px;font-size:14px;color:#ccc;}\n' +
+                '    .SubtitleBody.mobile .title{font-size:12px;}\n' +
+                '    .SubtitleBody .SubtitleTextBodyFrame{padding:0 10px;overflow-y:auto;position:absolute;top:8px;bottom:8px;}\n' +
+                '    .SubtitleBody .SubtitleTextBody{min-height:110px;font-size:14px;color:#ccc;}\n' +
+                '    .SubtitleBody.mobile .SubtitleTextBody{font-size:12px;}\n' +
+                '    .SubtitleBody .SubtitleTextBody p{margin-block-start:5px;margin-block-end:5px;}\n' +
+                '    .SubtitleBody .SubtitleTextBody p:first-of-type{color:#fff;font-size:20px;font-weight:bold;}\n' +
+                '    .SubtitleBody.mobile .SubtitleTextBody p:first-of-type{font-size:18px;}\n' +
+                '    .SubtitleBody.Fullscreen{position:absolute;left:20px;bottom:30px;z-index:50000;background-color:rgba(0, 0, 0, 0.6);width:800px;display:none}\n' +
+                '    .SubtitleBody.mobile.Fullscreen{width:300px;}\n' +
+                '    .is-fullScreenPage .SubtitleBody.Fullscreen,:root:-webkit-full-screen-ancestor  .SubtitleBody.Fullscreen{display:block;}\n' +
+                '    .is-fullScreenPage .SubtitleBody.Normalscreen{display:none;}\n' +
+                '    .invisibleDanmaku{opacity:0 !important;}\n' +
+                '    .BLDF .'+danmuClassName+' > div{opacity:'+(config.BLDFOtherDanmakuOpacity/100)+' !important;}\n' +
+                '    .BLDF .'+danmuClassName+' > div.matched-danmaku{'+
+                'opacity:'+(config.BLDFMatchedDanmakuOpacity/100)+' !important;'+
+                (config.BLDFMatchedDanmakuColor!=""?'color:'+config.BLDFMatchedDanmakuColor+' !important;':"")+
+                (config.BLDFMatchedDanmakuShadow!=""?'text-shadow: '+config.BLDFMatchedDanmakuShadow+' 1px 0px 1px, '+config.BLDFMatchedDanmakuShadow+' 0px 1px 1px, '+config.BLDFMatchedDanmakuShadow+' 0px -1px 1px, '+config.BLDFMatchedDanmakuShadow+' -1px 0px 1px !important;':"")+
+                '}\n' +
+                '    .SubtitleTextBodyFrame::-webkit-scrollbar {display: none;}' +
+                '    </style>');
+            $(".ChatToolBar").append('<div class="ChatEmotion" style="font-size: 16px;margin-top: -6px;" id="regexOn">💬</div>');
+            $(".ChatToolBar").append('<div id="regexSettings" style="font-size: 22px;margin-top: -16px;" class="ChatEmotion">⚙</div>');
+            if (config.BLDFNeedSubBody) {
+                $("#js-player-toolbar").before('<div class="SubtitleBody Normalscreen"><div style="height:100%;position:relative;"><div class="SubtitleTextBodyFrame"><div class="SubtitleTextBody"></div></div></div></div>');
+                $("#room-html5-player").append('<div class="SubtitleBody Fullscreen ui-resizable"><div style="height:100%;position:relative;"><div class="SubtitleTextBodyFrame"><div class="SubtitleTextBody"></div></div></div><div class="ui-resizable-handle ui-resizable-e" style="z-index: 90;"></div><div class="ui-resizable-handle ui-resizable-s" style="z-index: 90;"></div><div class="ui-resizable-handle ui-resizable-se ui-icon ui-icon-gripsmall-diagonal-se" style="z-index: 90;"></div></div>');
+                $(".SubtitleBody.Fullscreen").draggable();
+            }
+            var startInterval=function(){
+                if (config.BLDFRegex == null) return;
+                if (intervalID >= 0) clearInterval(intervalID);
+                BLDFReg = new RegExp(config.BLDFRegex);
+                intervalID = setInterval(function () {
+                    $("."+danmuClassName).children().each((i,obj)=>{
+                        if(!(obj.innerHTML.substr(-7) == "</span>")){
+                            var matchres;
+                            $(obj).children().each((i,o)=>{
+                                //console.log($(o).attr("class").trim());
+                                if($(o).attr("class").trim().substr(0,5)=="text-" || $(o).attr("class").trim().substr(0,6)=="super-")matchres=$(o).text();
+                            });
+                            console.log(matchres);
+                            matchres = matchres.match(BLDFReg);
+                            //console.log(obj.innerText);
+                            if (matchres != null && matchres != "") {
+                                //if (config.BLDFShowDanmaku) $(obj).removeClass("invisibleDanmaku");
+                                $(obj).addClass("matched-danmaku");
+                                //console.log(matchres);
+                                $('.SubtitleTextBody').prepend($("<p></p>").text(matchres));
+                                /*
+                                    $('.SubtitleTextBody').each(function (i, obj) {
+                                        $(obj).children().each(function (i, obj) {
+                                            if (i >= 6) {
+                                                //obj.remove();
+                                            }
+                                        });
+                                    });//*/
+                                if (config.BLDFShowMatchedDanmakuText) $(obj).children().each((i,o)=>{
+                                    //console.log($(o).attr("class").trim());
+                                    if($(o).attr("class").trim().substr(0,5)=="text-" || $(o).attr("class").trim().substr(0,6)=="super-")$(o).text(matchres);
+                                });
+                                if(config.BLDFRecord){
+                                    var ls=localStorage.getItem("record");
+                                    if (ls==null)ls=[];else ls=JSON.parse(ls);
+                                    ls.push({time:new Date().getTime(),text:matchres[0]});
+                                    localStorage.setItem("record",JSON.stringify(ls));
+                                }
+                            }
+                            obj.innerHTML = obj.innerHTML + '<span></span>';
+                        }
+                    });
+                }, config.BLDFIntervalDelay);
+            }
+            $("#regexSettings").click(function () {
+                window.open("https://yuyuyzl.github.io/BiliDMFilter/");
+            });
+
+            $("#regexOn").click(function () {
+                if (intervalID >= 0) {
+                    clearInterval(intervalID);
+                    $("body").removeClass("BLDF");
+                    $(".bilibili-danmaku").each(function (i, obj) {
+                        $(obj).removeClass("invisibleDanmaku");
+                    });
+                    intervalID=-1;
+                    $('.SubtitleTextBody').prepend("<p style='color: gray'>" + "弹幕过滤停止" + "</p>");
+                }
+                else {
+                    $("body").addClass("BLDF");
+                    $('.SubtitleTextBody').prepend("<p style='color: gray'>" + "弹幕过滤开始" + "</p>");
+                    startInterval();
+                }
+            });
+            if (config.BLDFAutoStart) {
+                $("#regexOn").click();
+            }
+        },100);
+
+    }else
+
 
     if(window.location.href.match(/.*\/BiliDMFilter\/.*/)){
         console.log(config);
